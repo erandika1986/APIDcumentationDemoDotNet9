9e3cfcf7869fc8e40f68d59d513eb54902946929
Erandika <erandika@solistech.com>
2025-02-28 09:00:34 +0530

    feat: initial commit.

---
+++ ApIDocumentationDemoDotNet9/SwaggerUI/Program.cs
@@ -0,0 +1,35 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "Version One");
+    });
+}
+
+app.UseHttpsRedirection();
+
+List<PersonRecord> people = [
+    new PersonRecord(1,"John", "Doe", "Erandika1986@gmail.com"),
+    new PersonRecord(2,"ABC", "Doe", "abc@gmail.com"),
+    new PersonRecord(3,"XYZ", "Doe", "xyz@gmail.com")
+    ];
+
+app.MapGet("/people", () => people);
+app.MapGet("/people/{id}", (int id) => people[id]);
+app.MapPost("/people", (PersonRecord person) => people.Add(person));
+app.MapDelete("/people/{id}", (int id) => people.RemoveAt(id));
+
+app.Run();
+
+
+public record PersonRecord(int Id, string FirstName, string LastName, string Email);
