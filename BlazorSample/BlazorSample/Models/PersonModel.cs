29439432213786fce200c8e22c8223402f5f3fed
Erandika <erandika@solistech.com>
2025-02-28 11:09:30 +0530

    feat: Initial commit.

---
+++ BlazorSample/BlazorSample/Models/PersonModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorSample.Models
+{
+    public class PersonModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateOnly? BirthDate { get; set; }
+        public string Gender { get; set; }
+    }
+}
